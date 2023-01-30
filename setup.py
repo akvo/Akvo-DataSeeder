2836b48ea3e2c7fd756c32e4dcc94039e611d412
wayangalihpratama <wgprtm@gmail.com>
2023-01-30 19:05:24 +0800

    [#1] Update setup, add docker

--- setup.py
+++ setup.py
@@ -1,53 +1,47 @@
 import setuptools
 
-with open('README.md', 'r', encoding='utf-8') as fh:
+with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='AkvoDataSeeder',
-    author='Akvo',
-    author_email='tech.consultancy@akvo.org',
-    description='Fast-API datapoints seeder',
-    keywords='akvo, data, helper, pypi, package',
+    name="AkvoDataSeeder",
+    author="Akvo",
+    author_email="tech.consultancy@akvo.org",
+    description="Fast-API Datapoint seeder",
+    keywords="akvo, data, helper, pypi, package",
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    url='https://github.com/akvo/Akvo-DataSeeder',
+    long_description_content_type="text/markdown",
+    url="https://github.com/akvo/Akvo-DataSeeder",
     project_urls={
-        'Documentation': 'https://github.com/akvo/Akvo-DataSeeder',
-        'Bug Reports':
-        'https://github.com/akvo/Akvo-DataSeeder/issues',
-        'Source Code': 'https://github.com/akvo/Akvo-DataSeeder',
+        "Documentation": "https://github.com/akvo/Akvo-DataSeeder",
+        "Bug Reports": "https://github.com/akvo/Akvo-DataSeeder/issues",
+        "Source Code": "https://github.com/akvo/Akvo-DataSeeder",
     },
-    package_dir={'': 'src'},
-    packages=setuptools.find_packages(where='src'),
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
     classifiers=[
         # https://pypi.org/classifiers/
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Build Tools',
-        'Topic :: Database :: Database Engines/Servers',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3 :: Only',
-        'Framework :: FastAPI',
-        'License :: OSI Approved :: GNU Affero General Public License v3',
-        'Operating System :: OS Independent',
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "Topic :: Database :: Database Engines/Servers",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3 :: Only",
+        "Framework :: FastAPI",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Operating System :: OS Independent",
     ],
-    python_requires='>=3.8.5',
-    install_requires=['pandas', 'pydantic'],
+    python_requires=">=3.8.5",
+    install_requires=["fastapi", "pydantic", "sqlalchemy"],
     extras_require={
-        'dev': ['check-manifest'],
-        # 'test': ['coverage'],
+        "dev": ["check-manifest"],
+        "test": ["httpx"],
     },
-    # entry_points={
-    #     'console_scripts': [  # for generate db etc
-    #         'migrate=migrate:main',
-    #          to run `main()` in src/migrate/__init__.py
-    #         'destroy=destroy:main',
-    #          to run `main()` in src/rollback/__init__.py
-    #         'generate_config=generate_config:main',
-    #          to run `main()` in src/generate_config/__init__.py
-    #     ],
-    # },
+    entry_points={
+        "console_scripts": [
+            "akvo-dataseeder=AkvoDataSeeder.cli.migrate:main"
+        ]
+    },
 )
